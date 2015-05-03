c494d6819591172c4a6fb387c9f39440f06b291f
Ryan <ryan@rcorre.net>
2015-05-02 22:16:07 -0400

    Clean game-specific code out of input manager.
    For now, just accept queries on keycodes directly without mapping to specific actions.

--- src/dau/input.d
+++ src/dau/input.d
@@ -5,24 +5,12 @@ import dau.setup;
 import dau.allegro;
 import dau.geometry.vector;
 
-private enum MouseButton {
+enum MouseButton : uint {
   lmb = 1,
   rmb = 2
 }
 
-private enum Keymap : uint[] {
-  left  = [ALLEGRO_KEY_A],
-  right = [ALLEGRO_KEY_D],
-  up    = [ALLEGRO_KEY_W],
-  down  = [ALLEGRO_KEY_S],
-
-  action1 = [ALLEGRO_KEY_Q],
-  action2 = [ALLEGRO_KEY_E],
-  undo    = [ALLEGRO_KEY_U],
-
-  skip  = [ALLEGRO_KEY_SPACE],
-  exit  = [ALLEGRO_KEY_ESCAPE],
-}
+alias KeyCode = uint;
 
 class InputManager {
   this() {
@@ -37,39 +25,6 @@ class InputManager {
     al_get_mouse_state(&_curMouseState);
   }
 
-  @property {
-    Vector2f scrollDirection() {
-      Vector2f scroll = Vector2f.zero;
-      if (keyHeld(Keymap.up)) {
-        scroll.y = -1;
-      }
-      else if (keyHeld(Keymap.down)) {
-        scroll.y = 1;
-      }
-      if (keyHeld(Keymap.left)) {
-        scroll.x = -1;
-      }
-      else if (keyHeld(Keymap.right)) {
-        scroll.x = 1;
-      }
-      return scroll;
-    }
-
-    bool selectUp()    { return keyPressed(Keymap.up); }
-    bool selectDown()  { return keyPressed(Keymap.down); }
-    bool selectLeft()  { return keyPressed(Keymap.left); }
-    bool selectRight() { return keyPressed(Keymap.right); }
-
-    bool skip()    { return keyPressed(Keymap.skip); }
-    bool exit()    { return keyPressed(Keymap.exit); }
-    bool undo()    { return keyPressed(Keymap.undo); }
-    bool action1() { return keyHeld(Keymap.action1); }
-    bool action2() { return keyHeld(Keymap.action2); }
-
-    bool select()  { return mouseClicked(MouseButton.lmb); }
-    bool inspect() { return mouseHeld(MouseButton.rmb); }
-  }
-
   Vector2i mousePos() {
     return Vector2i(_curMouseState.x, _curMouseState.y);
   }
@@ -78,29 +33,31 @@ class InputManager {
     return Vector2i(_prevMouseState.x, _prevMouseState.y);
   }
 
-  private:
-  bool keyHeld(Keymap buttons) {
-    return (cast(uint[]) buttons).any!(key => al_key_down(&_curKeyboardState, key));
+  bool keyHeld(KeyCode key) {
+    return al_key_down(&_curKeyboardState, key);
   }
 
-  bool keyPressed(Keymap buttons) {
-    return (cast(uint[]) buttons).any!(key => !al_key_down(&_prevKeyboardState, key) && al_key_down(&_curKeyboardState, key));
+  bool keyPressed(KeyCode key) {
+    return !al_key_down(&_prevKeyboardState, key) && al_key_down(&_curKeyboardState, key);
   }
 
-  bool keyReleased(int keycode) {
-    return al_key_down(&_prevKeyboardState, keycode) && !al_key_down(&_curKeyboardState, keycode);
+  bool keyReleased(KeyCode key) {
+    return al_key_down(&_prevKeyboardState, key) && !al_key_down(&_curKeyboardState, key);
   }
 
-  bool mouseClicked(MouseButton button) {
-    int b = cast(int) button;
+  bool mousePressed(MouseButton b) {
     return !al_mouse_button_down(&_prevMouseState, b) && al_mouse_button_down(&_curMouseState, b);
   }
 
-  bool mouseHeld(MouseButton button) {
-    int b = cast(int) button;
+  bool mouseReleased(MouseButton b) {
+    return al_mouse_button_down(&_prevMouseState, b) && !al_mouse_button_down(&_curMouseState, b);
+  }
+
+  bool mouseHeld(MouseButton b) {
     return al_mouse_button_down(&_curMouseState, b);
   }
 
+  private:
   ALLEGRO_KEYBOARD_STATE _curKeyboardState, _prevKeyboardState;
   ALLEGRO_MOUSE_STATE _curMouseState, _prevMouseState;
 }
