34b863bb21b05be3747d8269d60d6c8c3f82c550
Ryan <ryan@rcorre.net>
2015-05-09 18:43:18 -0400

    Rework color struct.
    Clean up old code, document, support json construction.

--- src/dau/graphics/color.d
+++ src/dau/graphics/color.d
@@ -1,63 +1,74 @@
 module dau.graphics.color;
 
-import std.algorithm, std.conv, std.string, std.array;
+import std.conv      : to;
+import std.array     : array;
+import std.string    : format, strip;
+import std.algorithm : splitter, map;
+import jsonizer;
 import dau.allegro;
 import dau.util.math;
 
-alias color = Color;
-
 struct Color {
-  ALLEGRO_COLOR color;
-  alias color this;
+  mixin JsonizeMe;
 
-  //TODO: opAssign from ALLEGRO_COLOR?
-  this(float r, float g, float b, float a = 1.0f) {
-    color = ALLEGRO_COLOR(r, g, b, a);
+  ALLEGRO_COLOR _color;
+  alias _color this;
+
+  /**
+   * Construct a color from a set of floats ranging from 0.0 to 1.0.
+   * Params:
+   *  r = red
+   *  g = green
+   *  b = blue
+   *  a = alpha
+   */
+  @jsonize this(float r, float g, float b, float a = 1.0f) {
+    _color = ALLEGRO_COLOR(r, g, b, a);
   }
 
+  /// parse color from string of form "r,g,b" or "r,g,b,a" with values given as floats
+  @jsonize this(string csv) {
+    auto vals = csv.splitter(",").map!(x => x.strip.to!float).array;
+    if (vals.length == 3) {
+      this(vals[0], vals[1], vals[2]);
+    }
+    else if (vals.length == 4) {
+      this(vals[0], vals[1], vals[2], vals[3]);
+    }
+    else {
+      assert(0, "failed to parse color from %s (%d entries)".format(csv, vals.length));
+    }
+  }
+
+  /**
+   * Construct a color from a set of unsigned bytes ranging from 0 to 255.
+   * Params:
+   *  r = red
+   *  g = green
+   *  b = blue
+   *  a = alpha
+   */
+  this(ubyte r, ubyte g, ubyte b, ubyte a = 255u) {
+    this(r / 255f, g / 255f, b / 255f, a / 255f);
+  }
+
+  /**
+   * Wrap an ALLEGRO_COLOR struct.
+   * Params:
+   *  color = an ALLEGRO_COLOR struct
+   */
   this(ALLEGRO_COLOR color) {
-    this.color = color;
+    _color = color;
   }
 
   static @property {
-    Color white() { return Color(1, 1, 1, 1); }
-    Color gray () { return Color(0.5, 0.5, 0.5, 1); }
-    Color black() { return Color(0, 0, 0, 1); }
-    Color red  () { return Color(1, 0, 0, 1); }
-    Color green() { return Color(0, 1, 0, 1); }
-    Color blue () { return Color(0, 0, 1, 1); }
-    Color clear() { return Color(0, 0, 0, 0); }
-  }
-}
-
-/// shortcut to create colors from unsigned byte values
-Color ucolor(ubyte r, ubyte g, ubyte b, ubyte a = 255u) {
-  return Color(r / 255f, g / 255f, b / 255f, a / 255f);
-}
-
-/// parse color from string of form "r,g,b" or "r,g,b,a" with values given as floats
-Color parseColor(string csvSpec) {
-  auto vals = csvSpec.splitter(",").map!(x => x.strip.to!float).array;
-  switch (vals.length) {
-    case 3:
-      return color(vals[0], vals[1], vals[2]);
-    case 4:
-      return color(vals[0], vals[1], vals[2], vals[3]);
-    default:
-      assert(0, "failed to parse color from %s (%d entries)".format(csvSpec, vals.length));
-  }
-}
-
-/// parse color from string of form "r,g,b" or "r,g,b,a" with values given as ubytes
-Color parseUColor(string csvSpec) {
-  auto vals = csvSpec.splitter(",").map!(x => x.strip.to!ubyte).array;
-  switch (vals.length) {
-    case 3:
-      return ucolor(vals[0], vals[1], vals[2]);
-    case 4:
-      return ucolor(vals[0], vals[1], vals[2], vals[3]);
-    default:
-      assert(0, "failed to parse color from %s (%d entries)".format(csvSpec, vals.length));
+    Color white() { return Color(1f  , 1f  , 1f  , 1f); }
+    Color gray () { return Color(0.5 , 0.5 , 0.5 , 1f); }
+    Color black() { return Color(0f  , 0f  , 0f  , 1f); }
+    Color red  () { return Color(1f  , 0f  , 0f  , 1f); }
+    Color green() { return Color(0f  , 1f  , 0f  , 1f); }
+    Color blue () { return Color(0f  , 0f  , 1f  , 1f); }
+    Color clear() { return Color(0f  , 0f  , 0f  , 0f); }
   }
 }
 
@@ -70,8 +81,8 @@ Color lerp(Color start, Color end, float factor) {
 }
 
 Color lerp(Color[] colors, float factor) {
-  if (colors.length == 2) { 
-    return lerp(colors[0], colors[1], factor); 
+  if (colors.length == 2) {
+    return lerp(colors[0], colors[1], factor);
   }
 
   float colorTime = 1.0 / (colors.length - 1); // time for each color pair
@@ -104,11 +115,11 @@ unittest {
   assert(approxEqual(c2, Color(0, 0, 0, 0.5)));
 
   // unsigned color with implied alpha
-  auto c3 = ucolor(100, 255, 0);
+  auto c3 = Color(100, 255, 0);
   assert(approxEqual(c3, Color(100 / 255f, 1, 0, 1)));
 
   // unsigned color with specified alpha
-  auto c4 = ucolor(0, 0, 255, 127);
+  auto c4 = Color(0, 0, 255, 127);
   assert(approxEqual(c4, Color(0, 0, 1, 127 / 255f)));
 
   auto c5 = Color.black.lerp(Color.white, 0.5);
@@ -121,10 +132,25 @@ unittest {
   assert(approxEqual(lerp([Color.black, Color.white, Color.red], 1), Color.red));
 
   // parsing from strings
-  assert(parseColor("1.0,0.5,0.2") == color(1.0, 0.5, 0.2));
-  assert(parseColor("1.0, 0.5, 0.2") == color(1.0, 0.5, 0.2));
-  assert(parseColor("1.0, 0.5, 0.2, 0.7") == color(1.0, 0.5, 0.2, 0.7));
-  assert(parseColor("1.0,0.5,0.2,0.7") == color(1.0, 0.5, 0.2, 0.7));
-  assert(parseColor("1,0,1") == color(1.0, 0.0, 1.0));
-  assert(parseUColor("255, 128, 64") == ucolor(255, 128, 64));
+  assert(approxEqual(Color("1,0,1")              , Color(1, 0, 1.0)));
+  assert(approxEqual(Color("1.0,0.5,0.2")        , Color(1, 0.5, 0.2)));
+  assert(approxEqual(Color("1.0, 0.5, 0.2")      , Color(1, 0.5, 0.2)));
+  assert(approxEqual(Color("1.0, 0.5, 0.2, 0.7") , Color(1, 0.5, 0.2, 0.7)));
+  assert(approxEqual(Color("1.0,0.5,0.2,0.7")    , Color(1, 0.5, 0.2, 0.7)));
+}
+
+/// Colors can be parsed from JSON
+unittest {
+  auto jstr = q{
+    [
+      {"r": 1.0, "g": 0.5, "b": 0.0},
+      "1.0, 0.5, 0.0",
+      "1.0, 0.5, 0.0, 0.5"
+    ]
+  };
+
+  auto colors = jstr.fromJSONString!(Color[]);
+  assert(colors[0] == Color(1.0, 0.5, 0.0));
+  assert(colors[1] == Color(1.0, 0.5, 0.0));
+  assert(colors[2] == Color(1.0, 0.5, 0.0, 0.5));
 }
