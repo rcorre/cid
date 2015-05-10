30fdb50367b3cbf2a611c10c5d94b60cff6a93af
Ryan <ryan@rcorre.net>
2015-05-09 22:27:27 -0400

    Clean up utilities.
    Consolidate func, enums, and range utilities into util.helpers.

--- src/dau/util/package.d
+++ src/dau/util/package.d
@@ -1,9 +1,7 @@
 module dau.util;
 
-public import dau.util.func;
 public import dau.util.math;
-public import dau.util.enums;
-public import dau.util.range;
 public import dau.util.config;
+public import dau.util.helpers;
 public import dau.util.bicycle;
 public import dau.util.droplist;
