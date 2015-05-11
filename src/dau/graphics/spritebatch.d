cd400177e84c5d4e15f1d95134ffa102bc777e8e
Ryan <ryan@rcorre.net>
2015-05-11 08:58:19 -0400

    Initial spritebatch implementation.

--- src/dau/graphics/spritebatch.d
+++ src/dau/graphics/spritebatch.d
@@ -1,55 +1,58 @@
 module dau.graphics.spritebatch;
 
-import std.container;
+import std.container : RedBlackTree;
 import dau.allegro;
 import dau.geometry;
 import dau.graphics.camera;
+import dau.graphics.bitmap;
 
-/*
 // TODO: group textures and use al_hold_bitmap_drawing
 class SpriteBatch {
   this() {
     _sprites = new SpriteStore;
   }
 
-  void draw(Sprite sprite, Vector2i pos, float angle = 0) {
-    if (sprite !is null) {
-      Entry entry;
-      entry.sprite = sprite;
-      entry.pos = pos;
-      entry.angle = angle;
-      _sprites.insert(entry);
-    }
+  void draw(T)(Bitmap bmp, Transform!T trans, Rect2 region, int depth = 0) {
+    Entry entry;
+    entry.bmp = bmp;
+    entry.region = region;
+    entry.tranform = trans;
+    entry.depth = depth;
+    _sprites.insert(entry);
   }
 
   void render(Camera camera) {
     // use camera transform, store previous transform
-    ALLEGRO_TRANSFORM prevTrans, curTrans;
+    ALLEGRO_TRANSFORM origTrans, baseTrans, entryTrans;
+    /* TODO: needed?
     int x, y, w, h; // prev clipping rect
-    al_copy_transform(&prevTrans, al_get_current_transform());
     al_get_clipping_rectangle(&x, &y, &w, &h);
-    al_copy_transform(&curTrans, camera.transform);
-    al_compose_transform(&curTrans, &prevTrans);
-    al_use_transform(&curTrans);
+    */
+    al_copy_transform(&origTrans, al_get_current_transform());
+    al_copy_transform(&baseTrans, &origTrans); // start with the current transform
+    al_compose_transform(&baseTrans, camera.transform); // compose with the camera transform
 
     foreach(entry ; _sprites) {
-      entry.sprite.draw(entry.pos, entry.angle);
+      al_copy_transform(&entryTrans, &baseTrans);
+      al_compose_transform(&entryTrans, entry.transform.transform);
+      al_use_transform(&entryTrans);
+      entry.bmp.drawRegion(entry.region);
     }
 
-    al_use_transform(&prevTrans); // restore old transform
-    al_set_clipping_rectangle(x, y, w, h);
+    al_use_transform(&origTrans); // restore old transform
+    //al_set_clipping_rectangle(x, y, w, h); TODO: needed?
     _sprites.clear();
   }
 
   private:
   struct Entry {
-    Sprite sprite;
-    Vector2i pos;
-    float angle;
+    Bitmap bmp;
+    Rect2i region;
+    Transform!float transform;
+    int depth;
   }
 
   // true indicates: allow duplicates
-  alias SpriteStore = RedBlackTree!(Entry, (a,b) => a.sprite.depth < b.sprite.depth, true);
+  alias SpriteStore = RedBlackTree!(Entry, (a,b) => a.depth < b.depth, true);
   SpriteStore _sprites;
 }
-*/
