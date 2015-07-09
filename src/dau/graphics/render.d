aeff0af15ca43152dce580c07a663c7adbc1c361
Ryan <ryan@rcorre.net>
2015-07-09 08:11:39 -0400

    Initial sprite batching implementation.
    Renderer now takes a range of sprites with the same bitmap and depth for the
    draw call to perform batching with al_hold_bitmap_drawing.

--- src/dau/graphics/render.d
+++ src/dau/graphics/render.d
@@ -1,58 +1,90 @@
 module dau.graphics.render;
 
-import std.container : RedBlackTree;
+import std.range     : only, isInputRange, ElementType;
+import std.typecons  : Proxy;
+import std.typetuple : TypeTuple;
+import std.container : SList, RedBlackTree;
 import dau.allegro;
 import dau.geometry;
-import dau.graphics.camera;
+import dau.graphics.font;
 import dau.graphics.color;
+import dau.graphics.camera;
 import dau.graphics.bitmap;
 
-// TODO: group textures and use al_hold_bitmap_drawing
 class Renderer {
-  this() {
-    _entries = new EntryStore;
+  private {
+    // true indicates: allow duplicates (entries at same depth)
+    alias Store = RedBlackTree!(SpriteBatch, (a,b) => a.depth < b.depth, true);
+    Store _batches;
   }
 
-  void draw(RenderInfo info) {
-    _entries.insert(info);
+  this() {
+    _batches = new Store;
   }
 
   void render() {
-    ALLEGRO_TRANSFORM origTrans, curTrans;
+    ALLEGRO_TRANSFORM origTrans;
     al_copy_transform(&origTrans, al_get_current_transform());
 
-    foreach(entry ; _entries) {
-      al_copy_transform(&curTrans, &origTrans);
-
-      if (entry.centered) {
-        al_translate_transform(&curTrans,
-            -entry.region.width / 2, -entry.region.height / 2);
-      }
-
-      al_compose_transform(&curTrans, entry.transform.transform);
-      al_use_transform(&curTrans);
-
-      entry.bmp.drawRegion(entry.region, entry.color, entry.flip);
+    foreach(batch ; _batches) {
+      al_hold_bitmap_drawing(true);
+      batch.draw(origTrans);
+      al_hold_bitmap_drawing(false);
     }
 
     al_use_transform(&origTrans); // restore old transform
-    _entries.clear();
+    _batches.clear();
   }
 
-  private:
+  void draw(Sprite sprite, Bitmap bmp, int depth = 0) {
+    draw(sprite.only, bmp, depth);
+  }
 
-  // true indicates: allow duplicates
-  alias EntryStore = RedBlackTree!(RenderInfo, (a,b) => a.depth < b.depth, true);
-  EntryStore _entries;
+  void draw(R)(R sprites, Bitmap bmp, int depth = 0)
+    if (isInputRange!R && is(ElementType!R == Sprite))
+  {
+    SpriteBatch sb;
+
+    sb.depth   = depth;
+    sb.bitmap  = bmp;
+    sb.sprites.insert(sprites);
+
+    _batches.insert(sb);
+  }
+
+  struct SpriteBatch {
+    int          depth;
+    Bitmap       bitmap;
+    SList!Sprite sprites;
+
+    void draw(in ALLEGRO_TRANSFORM origTrans) {
+      ALLEGRO_TRANSFORM curTrans;
+
+      foreach(sprite ; sprites) {
+        // start with the original transform
+        al_copy_transform(&curTrans, &origTrans);
+
+        if (sprite.centered) {
+          // translate by half the width and length to center the sprite
+          al_translate_transform(&curTrans,
+              -sprite.region.width / 2, -sprite.region.height / 2);
+        }
+
+        // compose with the transform for this individual sprite
+        al_compose_transform(&curTrans, sprite.transform.transform);
+
+        al_use_transform(&curTrans);
+
+        bitmap.drawRegion(sprite.region, sprite.color, sprite.flip);
+      }
+    }
+  }
 }
 
-/// Encompasses information needed to render a bitmap to the screen
-struct RenderInfo {
-  Bitmap           bmp;
-  Rect2i           region;
-  Transform!float  transform;
-  int              depth;
-  Bitmap.Flip      flip;
-  Color            color = Color.white;
-  bool             centered;
+struct Sprite {
+  Rect2i          region;
+  bool            centered;
+  Color           color;
+  Bitmap.Flip     flip;
+  Transform!float transform;
 }
