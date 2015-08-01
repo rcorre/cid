b96cfab3acf9c2a7f6624d1831bdb531189880ca
Ryan <ryan@rcorre.net>
2015-08-01 13:12:19 -0400

    Add unittest for input scheme mapping.

--- src/dau/events/input.d
+++ src/dau/events/input.d
@@ -1,10 +1,10 @@
 module dau.events.input;
 
 import std.conv      : to;
+import std.array     : array;
 import std.traits    : EnumMembers;
 import std.string    : toLower;
 import std.algorithm : map;
-import std.container : Array;
 import dau.allegro;
 import dau.events.keycodes;
 import jsonizer;
@@ -15,6 +15,8 @@ package {
 }
 
 struct ControlScheme {
+  mixin JsonizeMe;
+
   @jsonize {
     ButtonMap[string] buttons;
     AxisMap[string]   axes;
@@ -22,20 +24,28 @@ struct ControlScheme {
 }
 
 struct ButtonMap {
-  Array!int keys;
-  Array!int buttons;
+  mixin JsonizeMe;
+
+  int[] keys;
+  int[] buttons;
 
   @jsonize
   this(string[] keys, int[] buttons) {
     this.buttons = buttons;
-    this.keys = keys.map!(x => x.toLower.to!KeyCode.to!int);
+    this.keys = keys.map!(x => x.toLower.to!KeyCode.to!int).array;
   }
 }
 
 struct AxisMap {
+  mixin JsonizeMe;
+
   struct SubAxis {
-    int stick;
-    int axis;
+    mixin JsonizeMe;
+
+    @jsonize {
+      int stick;
+      int axis;
+    }
   }
 
   SubAxis          xAxis;
@@ -51,4 +61,48 @@ struct AxisMap {
     this.xAxis = axes["x"];
     this.yAxis = axes["y"];
   }
+}
+
+unittest {
+  import std.algorithm : equal;
+
+  auto json = `
+  {
+    "buttons": {
+      "confirm": {
+        "keys": [ "j", "enter", "space" ],
+          "buttons": [ 1, 2 ]
+      },
+        "cancel": {
+          "keys": [ "k", "escape" ],
+          "buttons": [ 3, 4 ]
+        }
+    },
+    "axes": {
+      "move": {
+        "keys": {
+          "up"   : "w",
+          "down" : "s",
+          "left" : "a",
+          "right": "d"
+        },
+        "axes": {
+          "x": { "stick": 0, "axis": 0 },
+          "y": { "stick": 0, "axis": 1 }
+        }
+      }
+    }
+  }`;
+
+  auto controls = json.fromJSONString!ControlScheme;
+
+  assert(controls.buttons["confirm"].keys[].equal(
+    [ ALLEGRO_KEY_J, ALLEGRO_KEY_ENTER, ALLEGRO_KEY_SPACE ]));
+
+  assert(controls.buttons["confirm"].buttons[].equal([ 1, 2]));
+
+  assert(controls.buttons["cancel"].keys[].equal(
+    [ ALLEGRO_KEY_K, ALLEGRO_KEY_ESCAPE ]));
+
+  assert(controls.buttons["cancel"].buttons[].equal([ 3, 4]));
 }
